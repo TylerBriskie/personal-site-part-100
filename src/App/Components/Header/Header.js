4ab4cad481afa116877510d8360d36df9d979b5c
Tyler Briskie <tylerbriskie@gmail.com>
2019-07-12 19:24:57 -0600

    header stuff

---
+++ src/App/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Header.scss';
+
+const Header = () => {
+    return <div>
+       <h1>I"M AN HEADER</h1>
+
+    </div>
+};
+
+export default Header;
