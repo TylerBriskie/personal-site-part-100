9ced05a30fad9d5f3300969e453d81fcc407fe88
Tyler Briskie <tylerbriskie@gmail.com>
2019-07-12 19:29:58 -0600

    footer stuff?

--- src/App/App.js
+++ src/App/App.js
@@ -2,25 +2,14 @@ import React from 'react';
 import logo from '../logo.svg';
 import './App.scss';
 import Header from './Components/Header/Header.js';
+import Footer from './Components/Footer/Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Footer />
     </div>
   );
 }
---
+++ src/App/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+    return <div>
+        <h1>I"M AN Footer ;)</h1>
+
+    </div>
+};
+
+export default Footer;
