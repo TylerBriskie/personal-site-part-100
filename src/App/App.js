018a4462a2f7e290c6ba572813b4a996fce66e85
Tyler Briskie <tylerbriskie@gmail.com>
2019-07-29 15:32:02 -0600

    sticky header

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.scss';
-import Header from './Components/Header/Header.js';
+// import Header from './Components/Header/Header.js';
 import Home from './Components/Home/Home.js';
 import Footer from './Components/Footer/Footer.js';
 
 function App() {
+
+    
   return (
     <div className="App">
-      <Header />
+      {/*<Header />*/}
       <Home />
       {/*<Resume />*/}
       {/*<Projects />*/}
--- src/App/Components/Home/Home.js
+++ src/App/Components/Home/Home.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import './Home.scss';
+import 'react-sticky-header/styles.css';
+import StickyHeader from 'react-sticky-header';
+
 const Home = () => {
     return <div className={"App-Home"}>
-        <button>More About Me</button>
+        <StickyHeader
+            // This is the sticky part of the header.
+            header={
+                <div className="Header_root">
+                    <h1 className="Header_title"> TYLER BRISKIE</h1>
+
+                    <ul className="Header_links">
+                        <li className="Header_link">Web Developer</li>
+                        <li className="Header_link">Musician</li>
+                        <li className="Header_link">About</li>
+                    </ul>
+                </div>
+            }
+        >
+            <section>
+                <image className={"app-hero-image"}>
+
+                </image>
+                <p>
+
+                </p>
+
+            </section>
+        </StickyHeader>
+
     </div>
 };
 
