b051d1fd4ff0289966aadc7322affe973b83a3fd
Tyler Briskie <tylerbriskie@gmail.com>
2019-07-29 15:59:05 -0600

    more sticky header scroll good times

--- src/App/Components/Home/Home.js
+++ src/App/Components/Home/Home.js
@@ -4,17 +4,22 @@ import 'react-sticky-header/styles.css';
 import StickyHeader from 'react-sticky-header';
 
 const Home = () => {
+
+    function scrollToProfile(){
+        console.log('i am scrolling');
+    }
     return <div className={"App-Home"}>
         <StickyHeader
             // This is the sticky part of the header.
             header={
                 <div className="Header_root">
                     <h1 className="Header_title"> TYLER BRISKIE</h1>
-
+                    <p>Web Developer.</p>
+                    <button onClick={scrollToProfile}>Nice to meet you</button>
                     <ul className="Header_links">
-                        <li className="Header_link">Web Developer</li>
-                        <li className="Header_link">Musician</li>
-                        <li className="Header_link">About</li>
+                        <li className="Header_link">WEB DEVELOPER</li>
+                        <li className="Header_link">MUSICIAN</li>
+                        <li className="Header_link">ABOUT</li>
                     </ul>
                 </div>
             }
