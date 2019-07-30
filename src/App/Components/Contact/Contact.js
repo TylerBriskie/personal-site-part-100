2f4933ea3e77f3305d4a0c3c9535329a56b497d5
Tyler Briskie <tylerbriskie@gmail.com>
2019-07-30 17:17:15 -0600

    update contact page and soundcloud icon

--- src/App/Components/Contact/Contact.js
+++ src/App/Components/Contact/Contact.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Contact.scss';
 import email from '../../../assets/email.svg';
 import linkedin from '../../../assets/linkedin.svg';
+import soundcloud from '../../../assets/soundcloud.svg';
 import octocat from '../../../assets/octocat.png';
 import ScrollAnimation from 'react-animate-on-scroll';
 
@@ -31,6 +32,13 @@ const Contact = () => {
                     <a target="_blank" href={"https://linkedin.com/in/tylerbriskie"}><img src={linkedin} alt="" /></a>
                 <a target="_blank" href={"https://linkedin.com/in/tylerbriskie"}>linkedin.com/in/tylerbriskie</a>
             </div>
+            </ScrollAnimation>
+            <ScrollAnimation animateIn={"fadeIn"} delay={750}>
+                <div className={"contact-info"}>
+
+                    <a target="_blank" href={"https://soundcloud.com/tbonebriskie"}><img src={soundcloud} alt="" /></a>
+                    <a target="_blank" href={"https://soundcloud.com/tbonebriskie"}>soundcloud.com/tbonebriskie</a>
+                </div>
             </ScrollAnimation>
         </div>
 
