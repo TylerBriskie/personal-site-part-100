59322c4c160e35986450e75dfb3f70d738dce067
Tyler Briskie <tylerbriskie@gmail.com>
2019-08-21 08:19:37 -0600

    added ipsum project

--- src/App/Components/Projects/Projects.js
+++ src/App/Components/Projects/Projects.js
@@ -4,6 +4,10 @@ import Section from '../Section/Section.js';
 import ScrollAnimation from 'react-animate-on-scroll';
 import ipsumImage1 from '../../../assets/ipsumProject.PNG';
 import ipsumImage2 from '../../../assets/ipsumProjectAddQuote.PNG';
+import nodeLogo from '../../../assets/nodejs-icon.svg';
+import mongoLogo from "../../../assets/mongoLogo.png";
+
+
 
 class Projects extends Component {
     render() {
@@ -13,8 +17,23 @@ class Projects extends Component {
 
                     <aside>
                         <h1>Propane Ipsum</h1>
+                        <a target="_blank" href={"https://propane-ipsum.herokuapp.com"}>
+                            <h5>
+                                Link to live app
+                            </h5>
+                        </a>
+                        <p>When I discovered that this didn't already exist I was sort of shocked.
+                            Part placeholder text generator, part quote database,
+                            Propane Ipsum is my love letter to Mike Judge's King of the Hill, my all time favorite comedy series. This project was made entirely in node.js, with a Mongo database and UI rendered by EJS.</p>
+                        <div className={"tech-stack"}>
+                            <ScrollAnimation animateIn={"fadeIn"} delay={666}>
+                                <img src={nodeLogo} alt=""/>
+                            </ScrollAnimation>
+                            <ScrollAnimation animateIn={"fadeIn"} delay={666}>
+                                <img src={mongoLogo} alt=""/>
+                            </ScrollAnimation>
 
-                        <p>Propane Ipsum is my love letter to Mike Judge's King of the Hill, possibly my favorite comedy television series of all time. This project was made entirely in node.js, with a Mongo database and UI rendered by EJS.</p>
+                        </div>
                     </aside>
                     <article>
                         <img src={ipsumImage1} alt="image 1"></img>
