83a875a5baa54bc0760d40719b087b8a4ffecc97
Tyler Briskie <tylerbriskie@gmail.com>
2019-08-20 18:13:21 -0600

    add propane ipsum section with some rudimentary styling

--- src/App/Components/Projects/Projects.js
+++ src/App/Components/Projects/Projects.js
@@ -2,18 +2,27 @@ import React, {Component} from 'react';
 import './Projects.scss';
 import Section from '../Section/Section.js';
 import ScrollAnimation from 'react-animate-on-scroll';
+import ipsumImage1 from '../../../assets/ipsumProject.PNG';
+import ipsumImage2 from '../../../assets/ipsumProjectAddQuote.PNG';
 
 class Projects extends Component {
     render() {
         return (<Section sectionName={"Projects"}>
             <section id={"Projects-Section"}>
-                <h1>Projects PAGE</h1>
+                <div className={"single-project-view"}>
 
-                <p>And now, in the spirit of the season: start shopping. And for every dollar of Krusty
-                    merchandise you buy, I will be nice to a sick kid. For legal purposes, sick kids may include
-                    hookers with a cold. Look out, Itchy! He's Irish!</p>
+                    <aside>
+                        <h1>Propane Ipsum</h1>
+
+                        <p>Propane Ipsum is my love letter to Mike Judge's King of the Hill, possibly my favorite comedy television series of all time. This project was made entirely in node.js, with a Mongo database and UI rendered by EJS.</p>
+                    </aside>
+                    <article>
+                        <img src={ipsumImage1} alt="image 1"></img>
+                        <img src={ipsumImage2} alt="image 2"/>
+                    </article>
+                </div>
+
             </section>
-
         </Section>)
     }
 }
